c9622d03a6655aecfa3a4b6444556527afcad198
donpi5 <bagy531hiro@gmail.com>
2019-12-19 12:55:31 +0000

    フォームの調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   has_many :works ,dependent: :destroy
   has_many :comments
+  has_many :likes
 
   attachment :image
 
