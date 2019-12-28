3f0fcf9a2fc28ded2fb8f863336b917fc551c8f0
donpi5 <bagy531hiro@gmail.com>
2019-12-28 11:53:59 +0000

    SNSシェアボタンを追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
     before_action :authenticate_user!
+    before_action :correct_user,only: [:edit,:update]
 
     def show
     	@user = User.find(params[:id])
@@ -25,5 +26,12 @@ class UsersController < ApplicationController
     private
     def user_params
     	params.require(:user).permit(:name,:email,:image,:introduction)
+    end
+
+    def correct_user
+        @user = User.find(params[:id])
+        unless @user == correct_user
+            redirect_to user_path(correct_user)
+        end
     end
 end
--- app/controllers/works_controller.rb
+++ app/controllers/works_controller.rb
@@ -1,4 +1,7 @@
 class WorksController < ApplicationController
+	before_action :authenticate_user!
+	before_action :correct_work,only: [:edit,:update]
+
 	def index
 		@works = Work.all.order(created_at: :desc)
 		@all_ranks = Work.find(Like.group(:work_id).order('count(work_id) desc').limit(6).pluck(:work_id))
@@ -39,6 +42,12 @@ class WorksController < ApplicationController
 		@comments = @work.comments
 	end
 
+	def destroy
+		work = Work.find(params[:id])
+		work.destroy
+		redirect_to works_path
+	end
+
 	def edit
 		@work = Work.find(params[:id])
 	end
@@ -56,5 +65,12 @@ class WorksController < ApplicationController
 	private
 	def work_params
 		params.require(:work).permit(:title,:description,:app_url,:image,:youtube_url, :tag_list)
+	end
+
+	def correct_work
+		@work = Work.find(params[:id])
+		if @work.user != current_user
+			redirect_to works_path
+		end
 	end
 end
